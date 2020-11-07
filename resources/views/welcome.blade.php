a7663aa8bcdf74001c4da9a7949a9bda59e3b70a
bayhaqqi-darmawan <59683593+bayhaqqi-darmawan@users.noreply.github.com>
2020-11-07 09:18:50 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -87,7 +87,7 @@
                 <div class="links">
                     <a href="https://laravel.com/docs">Bayhaqqi</a>
                     <a href="https://laracasts.com">Darmawan</a>
-                    <a href="https://laravel-news.com">News</a>
+                    <a href="https://laravel-news.com">DWDT04</a>
                     <a href="https://blog.laravel.com">Blog</a>
                     <a href="https://nova.laravel.com">Nova</a>
                     <a href="https://forge.laravel.com">Forge</a>
